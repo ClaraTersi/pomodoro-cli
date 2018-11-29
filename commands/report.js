46bda2801cda228ee0708475b6b921b1d8f03dff
Clara Tersi <claratersi@gmail.com>
2018-11-28 22:30:00 -0200

    find entries from a specific task

--- commands/report.js
+++ commands/report.js
@@ -1,13 +1,22 @@
 const { log } = console;
 const chalk = require('chalk');
 const { table } = require('table');
-const { listTasks } = require('../db');
+const { listTasks, getSingleTask } = require('../db');
 
-module.exports = () => {
-  const tasks = listTasks();
-  const data = [];
-  data.push([chalk.green.bold('TASK'), chalk.green.bold('DESCRIPTION'), chalk.green.bold('STARTED AT')]);
-  tasks.map(task => data.push([task.title, task.description, task.started_at]));
+const getTasks = (taskTitle) => {
+  let tasks = [];
+  if (taskTitle) {
+    tasks = getSingleTask(taskTitle);
+  } else {
+    tasks = listTasks();
+  }
+  return tasks;
+};
+
+const generateReport = (tasks) => {
+  const report = [];
+  report.push([chalk.green.bold('TASK'), chalk.green.bold('DESCRIPTION'), chalk.green.bold('STARTED AT')]);
+  tasks.map(task => report.push([task.id, task.description, task.started_at]));
   const config = {
     columns: {
       1: {
@@ -15,5 +24,10 @@ module.exports = () => {
       },
     },
   };
-  log(table(data, config));
+  return table(report, config);
+};
+
+module.exports = (taskTitle) => {
+  const tasks = getTasks(taskTitle);
+  log(generateReport(tasks));
 };
