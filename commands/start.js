479195ef3b4a429c32dbbc7e0a4f7cdb76938838
Clara Tersi <mclara.tersi@gmail.com>
2019-02-02 23:55:30 -0200

    changed task 'id' to task 'label''

--- commands/start.js
+++ commands/start.js
@@ -6,7 +6,7 @@ const { addTask } = require('../db');
 const { notify } = require('../notifications');
 
 const questions = [
-  { type: 'input', name: 'id', message: 'Identify the task you are working on:\n' },
+  { type: 'input', name: 'label', message: 'Identify the task you are working on:\n' },
   { type: 'input', name: 'description', message: 'Give a brief description of the task:\n' },
 ];
 
@@ -18,19 +18,19 @@ module.exports = () => {
     .then((answers) => {
       log(chalk.blue.bold('Pomodoro timer started!'), chalk.red('Press Ctrl+C to interrupt this task.'));
 
-      addTask(answers.id, answers.description);
+      addTask(answers.label, answers.description);
 
       process.on('SIGINT', () => {
         // TODO: record the time the task ended
-        log(chalk.red(`\nTask ${answers.id} interrupted.`));
+        log(chalk.red(`\nTask ${answers.label} interrupted.`));
         process.exit();
       });
 
       const bar = new ProgressBar(progressText, { total: 25, incomplete: ' ' });
-      bar.tick(0, { task: answers.id });
+      bar.tick(0, { task: answers.label });
 
       const timer = setInterval(() => {
-        bar.tick({ task: answers.id });
+        bar.tick({ task: answers.label });
         if (bar.complete) {
           notify();
           log(chalk.blue('Pomodoro finished!'));
